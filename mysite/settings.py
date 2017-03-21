dbe76f269c148c4117eb8084404e2e7e68dd8012
Stephen Ma <stephenma064@gmail.com>
2017-03-20 23:05:26 -0500

    add comma

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.WhiteNoiseMiddleware'
+    'django.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
