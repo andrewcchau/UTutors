14516fde36a177b80cedbb08c71f5ff3d5de8b1e
Stephen Ma <stephenma064@gmail.com>
2017-03-22 16:36:33 -0500

    change models to have relationships now

--- user/models.py
+++ user/models.py
@@ -10,4 +10,10 @@ class Tutor(models.Model):
     price = models.DecimalField
     rating = models.DecimalField
     bio = models.CharField(max_length=200, default='i am a tutor')
-    classes = models.CharField(max_length=200)
+    num_students = models.IntegerField
+    students = models.ManyToManyField(Student)
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+    tutors = models.ManyToManyField(Tutor)
+
