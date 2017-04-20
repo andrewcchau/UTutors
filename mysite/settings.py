bee7cd5a0003d0974cbff7c77cb8b15e9d9336f0
stephen <stephenma064@gmail.com>
2017-04-20 15:25:43 -0500

    little fi for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'mysite/static'),
 )
 
 SITE_ID = 2
